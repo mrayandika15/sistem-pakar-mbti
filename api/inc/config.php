504cccc7b05cbbfd5a4d7fa17bbeb5e4d4d06a16
Muhammad Rayandika <mrayandika15@gmail.com>
2021-05-29 23:43:56 +0700

    finish

--- api/inc/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbhost='localhost';
-$dbuser='root';
-$dbpass='';
-$dbname='mbti';
-$db=new mysqli($dbhost,$dbuser,$dbpass,$dbname);
-if ($db->connect_error) {
-    die('Connect Error ('.$db->connect_errno.') '. $db->connect_error);
-}
